f2b58f4d83c0ae39ae1c4d48814decd30dbc677f
luke0623 <luke0623@outlook.com>
2021-03-10 22:53:37 +0800

    fix 401
    fix 401

--- watchmen/common/deps.py
+++ watchmen/common/deps.py
@@ -27,11 +27,11 @@ def get_current_user(token: str = Depends(reusable_oauth2)
     # token_data = token.TokenPayload(**payload)
     except (jwt.JWTError, ValidationError):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
         )
     user = get_user(payload["sub"])
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
 
     return user
