022b16f910e47c909489323f7866de0466730cd0
luke0623 <luke0623@outlook.com>
2021-03-15 20:09:45 +0800

    Update space.py

--- watchmen/space/space.py
+++ watchmen/space/space.py
@@ -4,13 +4,9 @@ from watchmen.common.mongo_model import MongoModel
 
 
 class Space(MongoModel):
-    # id: int = Field( alias='_id')
+
     spaceId: str = None
     topicIds: List = None
     groupIds: List = None
     name: str = None
     description: str = None
-
-    # report_list: List = None
-    # status: bool = True
-    # accessUsers: List[str] = None
