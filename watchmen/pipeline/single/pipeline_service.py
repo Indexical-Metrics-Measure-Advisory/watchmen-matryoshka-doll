3be59ee02db5f84efd6e09ca87d2f3da45fa92f1
luke0623 <luke0623@outlook.com>
2021-03-26 16:29:14 +0800

    Update pipeline_service.py

--- watchmen/pipeline/single/pipeline_service.py
+++ watchmen/pipeline/single/pipeline_service.py
@@ -48,6 +48,8 @@ def __check_when_condition(children, data):
 
 def run_pipeline(pipeline: Pipeline, data):
     pipeline_status = PipelineRunStatus(pipelineId=pipeline.pipelineId,uid=get_surrogate_key(),start_time=datetime.now())
+    pipeline_status.oldValue = data[pipeline_constants.OLD]
+    pipeline_status.newValue = data[pipeline_constants.NEW]
 
     if pipeline.enabled:
         pipeline_topic = get_topic_by_id(pipeline.topicId)
@@ -98,4 +100,6 @@ def run_pipeline(pipeline: Pipeline, data):
             if pipeline_topic.kind is not None and pipeline_topic.kind == pipeline_constants.SYSTEM:
                 log.info("pipeline_status is {0}".format(pipeline_status))
             else:
+                # if pipeline_status.oldValue is not None:
+                #     print(pipeline_status.json())
                 sync_pipeline_monitor_data(pipeline_status)
