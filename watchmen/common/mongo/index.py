75a4060625563e9a8661591d0b353044c2ad5491
luke0623 <luke0623@outlook.com>
2021-04-27 14:11:27 +0800

    Update index.py

--- watchmen/common/mongo/index.py
+++ watchmen/common/mongo/index.py
@@ -9,13 +9,13 @@ from watchmen.common.storage.engine.storage_engine import get_client
 # from watchmen.common.storage.storage_template import drop_topic_data_table
 # from watchmen.common.utils.data_utils import build_collection_name
 
-client = get_client()
-
-collection_list_name = client.list_collection_names()
-
-
-def check_collection_if_exist(dbname, collection_name):
-    return collection_name in collection_list_name
+# client = get_client()
+#
+# collection_list_name = client.list_collection_names()
+#
+#
+# def check_collection_if_exist(dbname, collection_name):
+#     return collection_name in collection_list_name
 
 
 def delete_topic_collection(collection_name):
