71a067cb0458cdd3cd61bb4e22fd5b232fc78621
luke0623 <luke0623@outlook.com>
2021-07-22 15:28:01 +0800

    Update case_then_for_mapper.py

--- watchmen/pipeline/core/case/mapper/case_then_for_mapper.py
+++ watchmen/pipeline/core/case/mapper/case_then_for_mapper.py
@@ -8,7 +8,8 @@ from watchmen.pipeline.core.case.function.utils import parse_constant_expression
     get_variable_with_func_pattern, DOT, get_variable_with_dot_pattern
 from watchmen.pipeline.core.case.model.parameter import Parameter, ParameterJoint
 from watchmen.pipeline.core.parameter.utils import cal_factor_value
-from watchmen.pipeline.utils import get_factor
+from watchmen.pipeline.utils.units_func import get_factor
+
 from watchmen.report.model.column import Operator
 from watchmen.topic.storage.topic_schema_storage import get_topic_by_id
 
