e775a3488ba480ad7b9d1a58b12f3c870bed55fe
luke0623 <luke0623@outlook.com>
2021-08-21 16:23:10 +0800

    Update admin.py

--- watchmen/routers/admin.py
+++ watchmen/routers/admin.py
@@ -486,10 +486,9 @@ async def create_raw_topic_schema_v2(event: RawTopicGenerateEvent, current_user:
 ### LOG
 ## TODO tenantId for log
 @router.post("/pipeline/log/query", tags=["admin"])
-async def query_log_by_critical(query: MonitorLogQuery,current_user: User = Depends(deps.get_current_user)):
+async def query_log_by_critical(query: MonitorLogQuery, current_user: User = Depends(deps.get_current_user)):
     query_dict = {}
-    query_list = []
-    query_list.append({"tenant_id_",current_user.tenantId})
+    query_list = [{"tenant_id_", current_user.tenantId}]
     if query.criteria.topicId is not None:
         query_list.append({"topicId": query.criteria.topicId})
     if query.criteria.pipelineId is not None:
