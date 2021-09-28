ab3c0ef41b11f011f156de144ab882b72a872f54
luke0623 <luke0623@outlook.com>
2021-09-28 16:33:48 +0800

    update

--- watchmen/pipeline/index.py
+++ watchmen/pipeline/index.py
@@ -6,16 +6,5 @@ from watchmen.pipeline.model.trigger_type import TriggerType
 log = logging.getLogger("app." + __name__)
 
 
-def __match_trigger_type(trigger_type, pipeline):
-    if trigger_type == TriggerType.insert and (pipeline.type == "insert-or-merge" or pipeline.type == "insert"):
-        return True
-    elif trigger_type == TriggerType.update and (pipeline.type == "insert-or-merge" or pipeline.type == "update"):
-        return True
-    elif trigger_type == TriggerType.delete and pipeline.type == "delete":
-        return True
-    else:
-        return False
-
-
 def trigger_pipeline(topic_name, instance, trigger_type: TriggerType, current_user=None):
     trigger_pipeline_2(topic_name, instance, trigger_type, current_user)
