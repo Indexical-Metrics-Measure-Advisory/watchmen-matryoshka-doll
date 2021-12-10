ead36cd757712c9be87d20b31333ffa5ff70fddb
luke0623 <luke0623@outlook.com>
2021-12-10 16:03:27 +0800

    Update client.py

--- watchmen/common/dask/client.py
+++ watchmen/common/dask/client.py
@@ -1,6 +1,11 @@
+import dask
 from dask.distributed import Client
+
 from watchmen.config.config import settings
 from watchmen.database.singleton import singleton
+
+if settings.DASK_TEMP:
+    dask.config.set(temporary_directory=settings.DASK_TEMP)
 
 
 @singleton
@@ -8,10 +13,6 @@ class DaskClient:
 
     def __init__(self):
         self.client = Client(processes=settings.DASK_PROCESSES)
-
-
-if settings.DASK_TEMP:
-    dask.config.set(temporary_directory=settings.DASK_TEMP)
 
     def get_dask_client(self):
         return self.client
