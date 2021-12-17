0fa926ba47bd4926e53c1f2324f388bebbd73fa9
luke0623 <luke0623@outlook.com>
2021-12-17 20:20:24 +0800

    Update index.py

--- watchmen/monitor/prometheus/index.py
+++ watchmen/monitor/prometheus/index.py
@@ -3,4 +3,4 @@ from starlette_prometheus import metrics, PrometheusMiddleware
 
 def init_prometheus_monitor(app):
     app.add_middleware(PrometheusMiddleware)
-    app.add_route("/metrics/", metrics)
+    app.add_route("/metrics", metrics)
