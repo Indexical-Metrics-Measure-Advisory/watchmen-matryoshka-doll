7c80e772997b266ed54b947c201e212f24257a7b
luke0623 <luke0623@outlook.com>
2021-12-30 15:50:22 +0800

    fix data patch api

--- watchmen/routers/data_patch.py
+++ watchmen/routers/data_patch.py
@@ -10,6 +10,7 @@ from watchmen_boot.guid.snowflake import get_surrogate_key
 from watchmen.pipeline.core.context.pipeline_context import PipelineContext
 from watchmen.pipeline.core.worker.pipeline_worker import run_pipeline
 from watchmen.pipeline.storage.pipeline_storage import load_pipeline_by_topic_id
+from watchmen.pipeline.utils.units_func import add_audit_columns, INSERT, UPDATE
 from watchmen.topic.storage.topic_data_storage import find_topic_data_by_id_and_topic_name, \
     update_topic_instance, save_topic_instance
 from watchmen.topic.storage.topic_schema_storage import get_topic_by_name, get_topic
@@ -47,10 +48,12 @@ async def patch_topic_instance(topic_name, instance_id=None, instance=Body(...),
                                current_user: User = Depends(deps.get_current_user)):
     topic = get_topic_by_name(topic_name)
     if instance_id is None:
-        save_topic_instance(topic, instance, current_user)
+        add_audit_columns(instance,INSERT)
+        return save_topic_instance(topic, instance, current_user)
     else:
         result = find_topic_data_by_id_and_topic_name(topic, instance_id)
         if result is not None:
-            update_topic_instance(topic, instance, instance_id)
+            add_audit_columns(instance, UPDATE)
+            return update_topic_instance(topic, instance, instance_id)
         else:
             raise Exception("instance ID {0} could not find any data for update".format(instance_id))
