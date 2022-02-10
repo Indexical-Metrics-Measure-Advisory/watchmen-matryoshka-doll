64cfb53dfc35f1efa253a4d46ed354f7903fb67c
luke0623 <luke0623@outlook.com>
2022-02-10 10:31:02 +0800

    Update presto_fn.py

--- watchmen/common/presto/presto_fn.py
+++ watchmen/common/presto/presto_fn.py
@@ -17,3 +17,14 @@ class PrestoDay(Function):
     def __init__(self, date_part, alias=None):
         # date_part = getattr(date_part, "value", date_part)
         super(PrestoDay, self).__init__("DAY", date_part, alias=alias)
+
+class PrestoDayOfWeek(Function):
+    def __init__(self, date_part, alias=None):
+        # date_part = getattr(date_part, "value", date_part)
+        super(PrestoDay, self).__init__("DAY_OF_WEEK", date_part, alias=alias)
+
+
+class PrestoDayOfYear(Function):
+    def __init__(self, date_part, alias=None):
+        # date_part = getattr(date_part, "value", date_part)
+        super(PrestoDay, self).__init__("DAY_OF_YEAR", date_part, alias=alias)
